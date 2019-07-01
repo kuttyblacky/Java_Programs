ced51979311ff719ddb2c2e539feb755aa782153
test <test@DESKTOP-FUN3MTJ>
2019-07-01 13:09:18 +0530

    justfor refe

--- src/Java_Basics/findingnumberinNode.java
+++ src/Java_Basics/findingnumberinNode.java
@@ -16,13 +16,16 @@ public class findingnumberinNode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int[] temp={5,8,2,15,6,25,1,11,13};
-int k=11;
+int k=12;
 System.out.println(search(k));
 	}
 	 static boolean search(int k) {
 		boolean result=false;
-		Object temp;
-		//for(int i=)
+		int temp=5;
+		for(int i=0;i<temp;i++)
+		{
+			System.out.println("Test succcessfull");
+		}
 		
 		// TODO Auto-generated method stub
 		return result;
